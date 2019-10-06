0d8e49c6e13233a12319180f50ddcae07d72dc24
frank256 <39875787+frank256@users.noreply.github.com>
2019-10-06 11:00:48 +0200

    FlowLayout: code formatting.

--- src/lgui/layout/flowlayout.cpp
+++ src/lgui/layout/flowlayout.cpp
@@ -53,214 +53,197 @@
 
 namespace lgui {
 
-    FlowLayout::FlowLayout()
+FlowLayout::FlowLayout()
         : mmax_on_row(std::numeric_limits<int>::max()), mhorz_spacing(default_spacing),
           mvert_spacing(default_spacing), mhcenter(false),
-          msingle_row(false)
-    {}
+          msingle_row(false) {}
 
-    void FlowLayout::do_layout(const Rect& r)
-    {
-        if(!mtarget)
-            return;
+void FlowLayout::do_layout(const Rect& r) {
+    if (!mtarget)
+        return;
 
-        // remeasure?
+    // remeasure?
 
-        Size ts = r.size();
-        SizeConstraint wc = SizeConstraint(ts.w(), SizeConstraintMode::Exactly);
-        SizeConstraint hc = SizeConstraint(ts.h(), SizeConstraintMode::Exactly);
+    Size ts = r.size();
+    SizeConstraint wc = SizeConstraint(ts.w(), SizeConstraintMode::Exactly);
+    SizeConstraint hc = SizeConstraint(ts.h(), SizeConstraintMode::Exactly);
 
-        if(mtarget->needs_relayout() ||
-           (wc != mlast_wc || hc != mlast_hc)) {
-            measure(wc, hc);
-        }
-
-        for(auto& li : mitems) {
-            if(li.skip())
-                continue;
-            li.layout(li.allotted_rect().translated(r.pos()));
-        }
+    if (mtarget->needs_relayout() ||
+        (wc != mlast_wc || hc != mlast_hc)) {
+        measure(wc, hc);
     }
 
-    void FlowLayout::center_helper(std::vector<LayoutItem>& container,
-                                   std::vector<LayoutItem>::iterator it,
-                                   int offsx)
-    {
-        // We add offsx to all the items starting with it, then going back
-        // as long as y does not change.
+    for (auto& li : mitems) {
+        if (li.skip())
+            continue;
+        li.layout(li.allotted_rect().translated(r.pos()));
+    }
+}
 
-        // Deal with last item here, since reverse_iterator will point to element
-        // *before* it.
-        LayoutItem& last_item = *it;
-        last_item.set_allotted_pos(last_item.allotted_pos() + Position(offsx, 0));
-        int line_y = last_item.allotted_pos().y();
+void FlowLayout::center_helper(std::vector<LayoutItem>& container, std::vector<LayoutItem>::iterator it, int offsx) {
+    // We add offsx to all the items starting with it, then going back
+    // as long as y does not change.
 
-        std::vector<LayoutItem>::reverse_iterator rit(it);
-        while(rit != container.rend()) {
-            LayoutItem& item = *rit;
-            if(!item.skip()) {
-                if(item.allotted_pos().y() != line_y)
-                    break;
-                item.set_allotted_pos(item.allotted_pos()+Position(offsx, 0));
+    // Deal with last item here, since reverse_iterator will point to element
+    // *before* it.
+    LayoutItem& last_item = *it;
+    last_item.set_allotted_pos(last_item.allotted_pos() + Position(offsx, 0));
+    int line_y = last_item.allotted_pos().y();
+
+    std::vector<LayoutItem>::reverse_iterator rit(it);
+    while (rit != container.rend()) {
+        LayoutItem& item = *rit;
+        if (!item.skip()) {
+            if (item.allotted_pos().y() != line_y)
+                break;
+            item.set_allotted_pos(item.allotted_pos() + Position(offsx, 0));
+        }
+        ++rit;
+    }
+}
+
+
+MeasureResults FlowLayout::measure(SizeConstraint wc, SizeConstraint hc) {
+    // FIXME: Doesn't work?
+    mlast_wc = wc;
+    mlast_hc = hc;
+
+    DBG("MEASURE CALLED for with wc %d (%d), hc %d (%d) on %s\n", wc.value(), wc.mode(),
+        hc.value(), hc.mode(), mtarget ? mtarget->name().c_str() : "");
+
+    int longest_row = 0;
+    int max_height_on_row = 0;
+    int items_on_row = 0;
+    int y = 0, x = 0, taken_h = 0;
+
+    bool single_row = (is_single_row() || wc.mode() == SizeConstraintMode::NoLimits)
+                      && !(mmax_on_row < std::numeric_limits<int>::max());
+
+    SizeConstraint cwc = wc.adapted_for_child();
+    SizeConstraint chc = hc.adapted_for_child();
+
+    TooSmallAccumulator too_small;
+    for (auto _it = mitems.begin(); _it != mitems.end(); ++_it) {
+        LayoutItem& item = *_it;
+        if (item.skip())
+            continue;
+        if (single_row)
+            cwc = wc.adapted_for_child(x);
+        else
+            chc = hc.adapted_for_child(taken_h);
+
+        MeasureResults r;
+        Size s;
+        if (single_row || items_on_row < mmax_on_row) {
+            r = item.measure(cwc, chc);
+            s = too_small.consider(r);
+            DBG("Flow Measure1: %d, %d %s\n", s.w(), s.h(), item.name());
+        }
+
+        if (!single_row && (items_on_row >= mmax_on_row || (x + s.w() > wc.value()))) {
+            // New row.
+            if (mhcenter) {
+                // This could be done in layout().
+                int xtransl = (cwc.value() - x) / 2;
+                center_helper(mitems, _it, xtransl);
             }
-            ++rit;
+            if (x > 0) {
+                x -= mhorz_spacing;
+            }
+            longest_row = std::max(longest_row, x);
+            items_on_row = 0;
+            y += max_height_on_row + mvert_spacing;
+            taken_h = y;
+            x = 0;
+            max_height_on_row = 0;
+
+            // Need to remeasure every time?
+            chc = hc.adapted_for_child(taken_h);
+            r = item.measure(cwc, chc);
+            s = too_small.consider(r);
+            DBG("Flow Measure2: %d, %d %s\n", s.w(), s.h(), item.name());
+        }
+        item.set_allotted_rect(Rect(x, y, s));
+        DBG("Item rect %d, %d, %d, %d\n", x, y, s.w(), s.h());
+        items_on_row++;
+        x += s.w() + mhorz_spacing;
+        max_height_on_row = std::max(max_height_on_row, s.h());
+    }
+
+    if (msingle_row && mhcenter && !mitems.empty()) {
+        int xtransl = (wc.adapted_for_child().value() - x) / 2;
+        center_helper(mitems, mitems.begin() + (mitems.size() - 1), xtransl);
+    }
+    longest_row = std::max(longest_row, x);
+
+    y += max_height_on_row; // FIXME: double add?
+    DBG("MEASURE returns %d, %d on %s\n", longest_row, y,
+        mtarget ? mtarget->name().c_str() : "");
+    return force_size_constraints(Size(longest_row, y), wc, hc, too_small);
+}
+
+Size FlowLayout::min_size_hint() {
+    int wmax = 0, hmax = 0;
+    for (auto& it : mitems) {
+        Size msh = it.min_size_hint();
+        if (mmax_on_row == 1) {
+            wmax = std::max(msh.w(), wmax);
+            hmax += msh.h() + mvert_spacing;
+        }
+        else if (msingle_row) {
+            wmax += msh.w() + mhorz_spacing;
+            hmax = std::max(msh.h(), hmax);
+        }
+        else {
+            // FIXME: This is obviously not what we want.
+            wmax = std::max(msh.w(), wmax);
+            hmax = std::max(msh.h(), hmax);
         }
     }
+    return Size(wmax, hmax);
+}
 
+void FlowLayout::add_item(const LayoutItemProxy& elem) {
+    mitems.emplace_back(LayoutItem(elem));
+    added_elem(*elem.elem());
+}
 
-    MeasureResults FlowLayout::measure(SizeConstraint wc, SizeConstraint hc)
-    {
-        // FIXME: Doesn't work?
-        mlast_wc = wc;
-        mlast_hc = hc;
-
-        DBG("MEASURE CALLED for with wc %d (%d), hc %d (%d) on %s\n", wc.value(), wc.mode(),
-            hc.value(), hc.mode(), mtarget ? mtarget->name().c_str() : "");
-
-        int longest_row = 0;
-        int max_height_on_row = 0;
-        int items_on_row = 0;
-        int y  = 0, x = 0, taken_h = 0;
-
-        bool single_row = (is_single_row() || wc.mode() == SizeConstraintMode::NoLimits)
-                          && !(mmax_on_row < std::numeric_limits<int>::max());
-
-        SizeConstraint cwc = wc.adapted_for_child();
-        SizeConstraint chc = hc.adapted_for_child();
-
-        TooSmallAccumulator too_small;
-        for(auto _it = mitems.begin(); _it != mitems.end(); ++_it) {
-            LayoutItem& item = *_it;
-            if(item.skip())
-                continue;
-            if(single_row)
-                cwc = wc.adapted_for_child(x);
-            else
-                chc = hc.adapted_for_child(taken_h);
-
-            MeasureResults r;
-            Size s;
-            if(single_row || items_on_row < mmax_on_row) {
-                r = item.measure(cwc, chc);
-                s = too_small.consider(r);
-                DBG("Flow Measure1: %d, %d %s\n", s.w(), s.h(), item.name());
-            }
-
-            if(!single_row
-               && (items_on_row >= mmax_on_row
-                   || (x + s.w() > wc.value()))) {
-                // New row.
-                if(mhcenter) {
-                    // This could be done in layout().
-                    int xtransl = (cwc.value() - x) / 2;
-                    center_helper(mitems, _it, xtransl);
-                }
-                if(x > 0)
-                    x -= mhorz_spacing;
-                longest_row = std::max(longest_row, x);
-                items_on_row = 0;
-                y += max_height_on_row + mvert_spacing;
-                taken_h = y;
-                x = 0;
-                max_height_on_row = 0;
-
-                // Need to remeasure every time?
-                chc = hc.adapted_for_child(taken_h);
-                r = item.measure(cwc, chc);
-                s  = too_small.consider(r);
-                DBG("Flow Measure2: %d, %d %s\n", s.w(), s.h(), item.name());
-            }
-            item.set_allotted_rect(Rect(x, y, s));
-            DBG("Item rect %d, %d, %d, %d\n", x, y, s.w(), s.h());
-            items_on_row++;
-            x += s.w() + mhorz_spacing;
-            max_height_on_row = std::max(max_height_on_row, s.h());
-        }
-
-        if (msingle_row && mhcenter && !mitems.empty()) {
-            int xtransl = (wc.adapted_for_child().value() - x) / 2;
-            center_helper(mitems, mitems.begin()+(mitems.size()-1), xtransl);
-        }
-        longest_row = std::max(longest_row, x);
-
-        y += max_height_on_row; // FIXME: double add?
-        DBG("MEASURE returns %d, %d on %s\n", longest_row, y,
-                    mtarget ? mtarget->name().c_str() : "");
-        return force_size_constraints(Size(longest_row, y), wc, hc, too_small);
+void FlowLayout::remove_item(ILayoutElement& le) {
+    auto it = find_elem(le);
+    if (it != mitems.end()) {
+        mitems.erase(it);
+        removed_elem(le);
     }
+}
 
-    Size FlowLayout::min_size_hint()
-    {
-        int wmax = 0, hmax = 0;
-        for(auto& it : mitems) {
-            Size msh = it.min_size_hint();
-            if (mmax_on_row == 1) {
-                wmax = std::max(msh.w(), wmax);
-                hmax += msh.h() + mvert_spacing;
-            }
-            else if (msingle_row) {
-                wmax += msh.w() + mhorz_spacing;
-                hmax = std::max(msh.h(), hmax);
-            }
-            else {
-                // FIXME: This is obviously not what we want.
-                wmax = std::max(msh.w(), wmax);
-                hmax = std::max(msh.h(), hmax);
-            }
-        }
-        return Size(wmax, hmax);
-    }
+void FlowLayout::set_horizontal_spacing(int s) {
+    mhorz_spacing = s;
+    if (mtarget)
+        mtarget->request_layout();
+}
 
-    void FlowLayout::add_item(const LayoutItemProxy& elem)
-    {
-        mitems.emplace_back(LayoutItem(elem));
-        added_elem(*elem.elem());
-    }
+void FlowLayout::set_vertical_spacing(int s) {
+    mvert_spacing = s;
+    if (mtarget)
+        mtarget->request_layout();
+}
 
+void FlowLayout::set_single_row(bool s) {
+    msingle_row = s;
+    if (mtarget)
+        mtarget->request_layout();
+}
 
-    void FlowLayout::remove_item(ILayoutElement& le)
-    {
-        auto it = find_elem(le);
-        if(it != mitems.end()) {
-            mitems.erase(it);
-            removed_elem(le);
-        }
-    }
+void FlowLayout::set_horizontally_centered(bool hcenter) {
+    mhcenter = hcenter;
+    if (mtarget)
+        mtarget->request_layout();
+}
 
-
-    void FlowLayout::set_horizontal_spacing(int s)
-    {
-        mhorz_spacing = s;
-        if(mtarget)
-            mtarget->request_layout();
-    }
-
-    void FlowLayout::set_vertical_spacing(int s)
-    {
-        mvert_spacing = s;
-        if(mtarget)
-            mtarget->request_layout();
-    }
-
-    void FlowLayout::set_single_row(bool s)
-    {
-        msingle_row = s;
-        if(mtarget)
-            mtarget->request_layout();
-    }
-
-    void FlowLayout::set_horizontally_centered(bool hcenter)
-    {
-        mhcenter = hcenter;
-        if(mtarget)
-            mtarget->request_layout();
-    }
-
-    void FlowLayout::set_max_on_row(int mor)
-    {
-        mmax_on_row = mor;
-        if(mtarget)
-            mtarget->request_layout();
-    }
+void FlowLayout::set_max_on_row(int mor) {
+    mmax_on_row = mor;
+    if (mtarget)
+        mtarget->request_layout();
+}
 
 }
--- src/lgui/layout/flowlayout.h
+++ src/lgui/layout/flowlayout.h
@@ -41,77 +41,75 @@
 #define LGUI_FLOWLAYOUT_H
 
 #include "layoutitemcontainerbase.h"
-//#include "lgui/basiccontainer.h" // need the iterators
 #include <vector>
 
 namespace lgui {
-    /** This simple flow layout will arrange widgets from top-to-bottom, left-to-right in
-     *  a greedy manner. All widgets are offered all the horizontal space available unless
-     *  set_single_row() has been called with true.
-     *
-     *  You can also configure the spacing between widgets, set a maximum number of widgets
-     *  on one row or center the widgets horizontally.
-     */
-    class FlowLayout : public LayoutItemContainerBase<LayoutItem, std::vector<LayoutItem>>
-    {
-        protected:
-            void do_layout(const Rect& r) override;
+/** This simple flow layout will arrange widgets from top-to-bottom, left-to-right in
+ *  a greedy manner. All widgets are offered all the horizontal space available unless
+ *  set_single_row() has been called with true.
+ *
+ *  You can also configure the spacing between widgets, set a maximum number of widgets
+ *  on one row or center the widgets horizontally.
+ */
+class FlowLayout : public LayoutItemContainerBase<LayoutItem, std::vector<LayoutItem>> {
+    protected:
+        void do_layout(const Rect& r) override;
 
-        public:
-            FlowLayout();
+    public:
+        FlowLayout();
 
-            ~FlowLayout() override = default;
+        ~FlowLayout() override = default;
 
-            MeasureResults measure(SizeConstraint wc, SizeConstraint hc) override;
-            Size min_size_hint() override;
+        MeasureResults measure(SizeConstraint wc, SizeConstraint hc) override;
+        Size min_size_hint() override;
 
-            /** Add an item (widget or nested layout) to the layout. */
-            void add_item(const LayoutItemProxy& elem);
+        /** Add an item (widget or nested layout) to the layout. */
+        void add_item(const LayoutItemProxy& elem);
 
-            void remove_item(ILayoutElement& le);
+        void remove_item(ILayoutElement& le);
 
-            /** Return the horizontal spacing between widgets. */
-            int horizontal_spacing() const { return mhorz_spacing; }
+        /** Return the horizontal spacing between widgets. */
+        int horizontal_spacing() const { return mhorz_spacing; }
 
-            /** Return the vertical spacing between widgets. */
-            int vertical_spacing() const { return mvert_spacing; }
+        /** Return the vertical spacing between widgets. */
+        int vertical_spacing() const { return mvert_spacing; }
 
-            /** Set the horizontal spacing between widgets. */
-            void set_horizontal_spacing(int s);
+        /** Set the horizontal spacing between widgets. */
+        void set_horizontal_spacing(int s);
 
-            /** Set the vertical spacing between widgets. */
-            void set_vertical_spacing(int s);
+        /** Set the vertical spacing between widgets. */
+        void set_vertical_spacing(int s);
 
-            /** Set whether the flow layout should keep its layout items on a single row. */
-            void set_single_row(bool s);
+        /** Set whether the flow layout should keep its layout items on a single row. */
+        void set_single_row(bool s);
 
-            /** Return whether the flow layout keeps its layout items on a single row. */
-            bool is_single_row() const { return msingle_row; }
+        /** Return whether the flow layout keeps its layout items on a single row. */
+        bool is_single_row() const { return msingle_row; }
 
 
-            /** Return whether the flow layout centers its items horizontally on each row. */
-            bool is_horizontally_centered() const { return mhcenter; }
-            /** Set whether the flow layout will center its items horizontally on each row. */
-            void set_horizontally_centered(bool hcenter);
+        /** Return whether the flow layout centers its items horizontally on each row. */
+        bool is_horizontally_centered() const { return mhcenter; }
+        /** Set whether the flow layout will center its items horizontally on each row. */
+        void set_horizontally_centered(bool hcenter);
 
-            /** Return the maximum number of layout items allowed on one row. */
-            int max_on_row() const { return mmax_on_row; }
-            /** Set the maximum number of layout items allowed on one row. If set to 1, every item will reside
-                on its own row. */
-            void set_max_on_row(int mor);
+        /** Return the maximum number of layout items allowed on one row. */
+        int max_on_row() const { return mmax_on_row; }
+        /** Set the maximum number of layout items allowed on one row. If set to 1, every item will reside
+            on its own row. */
+        void set_max_on_row(int mor);
 
-         private:
-            int mmax_on_row;
-            int mhorz_spacing, mvert_spacing;
-            bool mhcenter, msingle_row;
-            SizeConstraint mlast_wc, mlast_hc;
+    private:
+        int mmax_on_row;
+        int mhorz_spacing, mvert_spacing;
+        bool mhcenter, msingle_row;
+        SizeConstraint mlast_wc, mlast_hc;
 
-            static void center_helper(std::vector<LayoutItem>& container,
-                                      std::vector<LayoutItem>::iterator it,
-                                      int offsx);
+        static void center_helper(std::vector<LayoutItem>& container,
+                                  std::vector<LayoutItem>::iterator it,
+                                  int offsx);
 
-            static const int default_spacing = 4;
-    };
+        static const int default_spacing = 4;
+};
 
 }
 
