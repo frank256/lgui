1d6b0406daaec75e285504502b41d162d14b63f2
frank256 <39875787+frank256@users.noreply.github.com>
2020-07-19 13:40:01 +0200

    Fix color of test, add license header.

--- src/tests/layoutanimationtest.cpp
+++ src/tests/layoutanimationtest.cpp
@@ -1,6 +1,46 @@
+/*   _                _
+*   | |              (_)
+*   | |  __ _  _   _  _
+*   | | / _` || | | || |
+*   | || (_| || |_| || |
+*   |_| \__, | \__,_||_|
+*        __/ |
+*       |___/
+*
+* Copyright (c) 2015-20 frank256
+*
+* License (BSD):
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice, this
+*    list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice, this
+*    list of conditions and the following disclaimer in the documentation and/or
+*    other materials provided with the distribution.
+*
+* 3. Neither the name of the copyright holder nor the names of its contributors may
+*    be used to endorse or promote products derived from this software without
+*    specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+* OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+* THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include "layoutanimationtest.h"
 #include "lgui/drawevent.h"
 #include "lgui/platform/graphics.h"
+#include "lgui/style/style.h"
 
 LayoutAnimationTestRow::LayoutAnimationTestRow() {
     madd_button.set_text("Add");
@@ -48,7 +88,7 @@ void LayoutAnimationTestRow::add_button() {
 }
 
 void LayoutAnimationTestRow::draw_background(const lgui::DrawEvent& de) const {
-    de.gfx().rect(size_rect(), lgui::grey(1.0), 1);
+    de.gfx().rect(size_rect(), style().button_text_color(lgui::WidgetState(false, false, false), 1.0), 1);
 }
 
 void LayoutAnimationTestRow::cleanup() {
